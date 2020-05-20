30c2fe1d4f1a7bd7cd1035e04d70101e7c565b74
Kornpob Bhirombhakdi <kbhirombhakdi@altalune.local.stsci.edu>
2020-05-20 14:42:19 -0400

    v.2.3.0

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # axehelper
-axehelper is a python package to use with aXe.
+axehelper is a python package to use with aXe. Most of the routines work with Python 3.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="axehelper",
-    version="1.1.0",
+    version="2.3.0",
     author="Kornpob Bhirombhakdi",
     author_email="kbhirombhakdi@stsci.edu",
     long_description=open("README.md").read(),
@@ -10,9 +10,9 @@ setuptools.setup(
     url="https://github.com/bkornpob/axehelper",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 2"
+        "Programming Language :: Python :: 3"
         ,"License :: OSI Approved :: MIT License"
         ,"Operating System :: OS Independent"
     ],
-    python_requires='>=2.7'
+    python_requires='>=3.'
 )
